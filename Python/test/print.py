c5a1e2cae1eb950978c00b0d989780d654b558ea
cat <921660758@qq.com>
2020-04-11 02:52:16 +0800

    python3 中 print 后面需要用括号把参数包含

--- Python/test/print.py
+++ Python/test/print.py
@@ -4,16 +4,10 @@ x='a'
 y='b'
 
 # 换行输出
-print x
-print y
+print(x)
+print(y)
 
-print '--------------'
-
-# 不换行输出
-print x,
-print y
-
-print '--------------'
+print('--------------')
 
 # 不换行输出
-print x,y
+print(x,y)
--- Python/test/str.py
+++ Python/test/str.py
@@ -2,9 +2,9 @@
 
 str = u'我是你大爷'
 
-print str
-print str[0]
-print str[2:5]
-print str[2:]
-print str * 2
-print str + '?'
+print(str)
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str * 2)
+print(str + '?')
