269811de42e0e112657c0e92bca5e3380dcde59b
cat <921660758@qq.com>
2020-04-12 03:27:41 +0800

    for循环的迭代器原理

---
+++ Python/test/for.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+class test():
+    def __init__(self,data=1):
+        self.data = data
+
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.data > 5:
+            raise StopIteration
+        else:
+            self.data+=1
+            return self.data
+
+for item in test(3):
+    print(item)
