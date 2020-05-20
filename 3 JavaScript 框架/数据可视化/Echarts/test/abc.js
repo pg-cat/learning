c7860f5d7ee54b90cd62efd9a6310a92bad303bc
cat <921660758@qq.com>
2020-05-21 04:55:40 +0800

    echarts

--- 3 JavaScript 框架/数据可视化/Echarts/test/abc.js
+++ 3 JavaScript 框架/数据可视化/Echarts/test/abc.js
@@ -1,16 +1,35 @@
+var myChart = echarts.init(document.getElementById('main'));
+// 显示标题，图例和空的坐标轴
 myChart.setOption({
-  series: [
-    {
-      name: '访问来源',
-      type: 'pie',
-      radius: '55%',
-      data: [
-        { value: 235, name: '视频广告' },
-        { value: 274, name: '联盟广告' },
-        { value: 310, name: '邮件营销' },
-        { value: 335, name: '直接访问' },
-        { value: 400, name: '搜索引擎' }
-      ]
-    }
-  ]
-})
+  title: {
+    text: '异步数据加载示例'
+  },
+  tooltip: {},
+  legend: {
+    data: ['销量']
+  },
+  xAxis: {
+    data: []
+  },
+  yAxis: {},
+  series: [{
+    name: '销量',
+    type: 'bar',
+    data: []
+  }]
+});
+
+// 异步加载数据
+$.get('data.json').done(function (data) {
+  // 填入数据
+  myChart.setOption({
+    xAxis: {
+      data: data.categories
+    },
+    series: [{
+      // 根据名字对应到相应的系列
+      name: '销量',
+      data: data.data
+    }]
+  });
+});
